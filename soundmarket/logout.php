4c7c8cb6186da20b2251dc6d2846cb707f29f78e
nikito4 <nikolakostadinov26@itpg-varna.bg>
2026-04-14 21:17:53 +0300

    Delete soundmarket directory

--- soundmarket/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/inc/config.php';
-require_once __DIR__ . '/inc/auth.php';
-logout_user();
-header('Location: ' . APP_BASE . '/index.php');
-exit;
